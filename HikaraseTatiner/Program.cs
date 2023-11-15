ac926628405bfd9cac2c3201c671edfc6aa41b19
koudenpa <koudenpa@hotmail.com>
2023-11-15 23:58:39 +0900

    README

--- HikaraseTatiner/Program.cs
+++ HikaraseTatiner/Program.cs
@@ -9,8 +9,3 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
-
-// https://github.com/shimat/opencvsharp_blazor_sample
-/*
-dotnet workload install wasm-tools
- */
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # HikaraseTatiner
+
+プラモの写真を光らせたい。という気持ちで生えたリポジトリ。
+
+OpenCV + Blazor WebAssembly でどうにかならないものか画策している。
+
+
+## 参考
+
+- https://github.com/shimat/opencvsharp_blazor_sample
